fd4fe397414ea38afc6d858dc9b698b8b845f9c9
devopMarkz <marcosdev2002@gmail.com>
2025-07-24 08:45:33 -0300

    Criando testes para validar regras de negocio das entidades

--- Microsservices/Delivery-Tracking/src/test/java/com/algaworks/algadelivery/delivery/tracking/domain/model/DeliveryStatusTest.java
+++ Microsservices/Delivery-Tracking/src/test/java/com/algaworks/algadelivery/delivery/tracking/domain/model/DeliveryStatusTest.java
@@ -1,4 +1,26 @@
+package com.algaworks.algadelivery.delivery.tracking.domain.model;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
 import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
 class DeliveryStatusTest {
-  
+
+    @Test
+    void draft_canChangeToWaitingForCourier() {
+        assertTrue(
+                DeliveryStatus.DRAFT.canChangeTo(DeliveryStatus.WAITING_FOR_COURIER)
+        );
+    }
+
+    @Test
+    void draft_canChangeToInTransit() {
+        assertTrue(
+                DeliveryStatus.DRAFT.canNotChangeTo(DeliveryStatus.IN_TRANSIT)
+        );
+    }
+
 }
--- Microsservices/Delivery-Tracking/src/test/java/com/algaworks/algadelivery/delivery/tracking/domain/model/DeliveryTest.java
+++ Microsservices/Delivery-Tracking/src/test/java/com/algaworks/algadelivery/delivery/tracking/domain/model/DeliveryTest.java
@@ -1,4 +1,51 @@
+package com.algaworks.algadelivery.delivery.tracking.domain.model;
+
+import com.algaworks.algadelivery.delivery.tracking.domain.exception.DomainException;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.math.BigDecimal;
+import java.time.Duration;
+
 import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
 class DeliveryTest {
-  
+
+    @Test
+    public void shouldChangeStatusToPlaced(){
+        Delivery delivery = Delivery.draft();
+
+        delivery.editPreparationDetails(createdValidPreparationDetails());
+
+        delivery.place();
+
+        assertEquals(DeliveryStatus.WAITING_FOR_COURIER, delivery.getStatus());
+        assertNotNull(delivery.getPlacedAt());
+    }
+
+    @Test
+    public void shouldNotPlace(){
+        Delivery delivery = Delivery.draft();
+
+        assertThrows(DomainException.class, delivery::place);
+
+        assertEquals(DeliveryStatus.DRAFT, delivery.getStatus());
+        assertNull(delivery.getPlacedAt());
+    }
+
+    private Delivery.PreparationDetails createdValidPreparationDetails() {
+        ContactPoint sender = new ContactPoint("00000-000", "Rua São Paulo", "100", "Sala 501", "João Silva", "(98) 98606-3363");
+        ContactPoint recipient = new ContactPoint("12345-000", "Rua São Luís", "200", "Sala 601", "Marcos André", "(98) 98809-6094");
+        return Delivery.PreparationDetails.builder()
+                .sender(sender)
+                .recipient(recipient)
+                .distanceFee(new BigDecimal("15.00"))
+                .courierPayout(new BigDecimal("5.00"))
+                .expectedDeliveryTime(Duration.ofHours(5))
+                .build();
+    }
+
+
 }
