fda5be92ebe0b4675a24869be2ff8ab06f2ca257
devopMarkz <marcosdev2002@gmail.com>
2025-07-24 23:32:59 -0300

    Criando camada de repositories

---
+++ Microsservices/Delivery-Tracking/src/test/java/com/algaworks/algadelivery/delivery/tracking/domain/repository/DeliveryRepositoryTest.java
@@ -0,0 +1,51 @@
+package com.algaworks.algadelivery.delivery.tracking.domain.repository;
+
+import com.algaworks.algadelivery.delivery.tracking.domain.model.ContactPoint;
+import com.algaworks.algadelivery.delivery.tracking.domain.model.Delivery;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.math.BigDecimal;
+import java.time.Duration;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+class DeliveryRepositoryTest {
+
+    @Autowired
+    private DeliveryRepository deliveryRepository;
+
+    @Test
+    public void shouldPersist(){
+        Delivery delivery = Delivery.draft();
+
+        delivery.editPreparationDetails(createdValidPreparationDetails());
+
+        delivery.addItem("Computador", 2);
+        delivery.addItem("Notebook", 2);
+
+        deliveryRepository.saveAndFlush(delivery);
+
+        Delivery persistedDelivery = deliveryRepository.findById(delivery.getId()).orElseThrow();
+
+        assertEquals(2, delivery.getItems().size());
+    }
+
+    private Delivery.PreparationDetails createdValidPreparationDetails() {
+        ContactPoint sender = new ContactPoint("00000-000", "Rua São Paulo", "100", "Sala 501", "João Silva", "(98) 98606-3363");
+        ContactPoint recipient = new ContactPoint("12345-000", "Rua São Luís", "200", "Sala 601", "Marcos André", "(98) 98809-6094");
+        return Delivery.PreparationDetails.builder()
+                .sender(sender)
+                .recipient(recipient)
+                .distanceFee(new BigDecimal("15.00"))
+                .courierPayout(new BigDecimal("5.00"))
+                .expectedDeliveryTime(Duration.ofHours(5))
+                .build();
+    }
+
+
+}
